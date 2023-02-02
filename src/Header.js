fd6e7932c312afcb53e4c6be791636e8df144dd6
avishaig1986 <avishayg1986@gmail.com>
2023-02-02 02:14:16 +0200

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    const headerStyle = {
+        backgroundColor: "royalBlue",
+        color: '#fff'
+    }
+    return (
+        <header style={headerStyle} >
+            <h1>Groceries List</h1>
+        </header>
+      )
+
+}
+
+export default Header
